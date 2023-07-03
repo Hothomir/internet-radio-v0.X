92649bce3ffba081489a56ffbcd29e9d361d768c
Bartosz Jaskulski <jaskula242@gmail.com>
2023-07-03 18:27:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+#!/usr/bin/env python
+import urllib2
+
+stream_url = 'https://stream.skylab-radio.com/live'
+request = urllib2.Request(stream_url)
+try:
+    request.add_header('Icy-MetaData', 1)
+    response = urllib2.urlopen(request)
+    icy_metaint_header = response.headers.get('icy-metaint')
+    if icy_metaint_header is not None:
+        metaint = int(icy_metaint_header)
+        read_buffer = metaint+255
+        content = response.read(read_buffer)
+        title = content[metaint:].split("'")[1]
+        print(title)
+except:
+    print('Error!')
