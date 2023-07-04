01d59f7caae1c3bfea19936a8110a5ac3f383713
Bartosz Jaskulski <jaskula242@gmail.com>
2023-07-03 21:01:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -63,34 +63,22 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
+def title_printer(client: miniaudio.IceCastClient, new_title: str) -> None:
+    print("Stream title: ", new_title)
+
+with miniaudio.IceCastClient("https://stream.skylab-radio.com/live", update_stream_title=title_printer) as source:
+    print("Connected to internet stream, audio format:", source.audio_format.name)
+    station_name = "Station name: " + source.station_name
+    print("Press <enter> to quit playing.\n")
+    stream = miniaudio.stream_any(source, source.audio_format)
+    
+with miniaudio.PlaybackDevice() as device:
+    device.start(stream)
+    input()   # wait for user input, stream plays in background
 
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-    def title_printer(client: miniaudio.IceCastClient, new_title: str) -> None:
-    print("Stream title: ", new_title)
-
-    with miniaudio.IceCastClient("https://stream.skylab-radio.com/live", update_stream_title=title_printer) as source:
-        print("Connected to internet stream, audio format:", source.audio_format.name)
-        station_name = "Station name: " + source.station_name
-        print("Press <enter> to quit playing.\n")
-        stream = miniaudio.stream_any(source, source.audio_format)
-    
-    with miniaudio.PlaybackDevice() as device:
-        device.start(stream)
-        input()   # wait for user input, stream plays in background
-
-    cmd = "hostname -I | cut -d' ' -f1"
-    IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
-    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU Temp: %.1f C\", $(NF-0) / 1000}'"  # pylint: disable=line-too-long
-    Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
     # Write four lines of text.
     y = top
