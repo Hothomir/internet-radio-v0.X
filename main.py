114d49cc57781b8b11d6364c6720958205b906a0
Bartosz Jaskulski <jaskula242@gmail.com>
2023-07-04 13:04:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,6 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
-def title_printer(client: miniaudio.IceCastClient, new_title: str) -> None:
-    print("Stream title: ", new_title)
 
 with miniaudio.IceCastClient("https://stream.skylab-radio.com/live", update_stream_title=title_printer) as source:
     print("Connected to internet stream, audio format:", source.audio_format.name)
