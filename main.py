71f9fcaa38afba5f97573c54dd842a9227f509aa
Bartosz Jaskulski <jaskula242@gmail.com>
2023-07-04 13:54:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ from adafruit_rgb_display import st7789
 def title_printer(client: miniaudio.IceCastClient, new_title: str) -> None:
     print("Stream title: ", new_title)
 
+def stream_access():
+    with miniaudio.IceCastClient("https://stream.skylab-radio.com/live", update_stream_title=title_printer) as source:
+        print("Connected to internet stream, audio format:", source.audio_format.name)
+        station_name = "Station name: " + source.station_name
+        print("Press <enter> to quit playing.\n")
+        stream = miniaudio.stream_any(source, source.audio_format)
+        
+    with miniaudio.PlaybackDevice() as device:
+        device.start(stream)
+        input()   # wait for user input, stream plays in background
+
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
 dc_pin = digitalio.DigitalInOut(board.D25)
@@ -64,25 +75,15 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
-with miniaudio.IceCastClient("https://stream.skylab-radio.com/live", update_stream_title=title_printer) as source:
-    print("Connected to internet stream, audio format:", source.audio_format.name)
-    station_name = "Station name: " + source.station_name
-    print("Press <enter> to quit playing.\n")
-    stream = miniaudio.stream_any(source, source.audio_format)
-    
-with miniaudio.PlaybackDevice() as device:
-    device.start(stream)
-    input()   # wait for user input, stream plays in background
-
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
     # Write four lines of text.
     y = top
-    draw.text((x, y), station_name, font=font, fill="#FFFFFF")
-    y += font.getsize(station_name)[1]
-    draw.text((x, y), new_title, font=font, fill="#FFFFFF")
+    draw.text((x, y), stream_access.station_name, font=font, fill="#FFFFFF")
+    y += font.getsize(stream_access.station_name)[1]
+    draw.text((x, y), stream_access.new_title, font=font, fill="#FFFFFF")
 
     # Display image.
     disp.image(image, rotation)
